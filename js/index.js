464fe97bba0e40b13ee3c1067e0e71dce725dd04
davidreke <54542172+davidreke@users.noreply.github.com>
2019-10-04 14:25:39 -0500

    Nearly finished
    Added background and faces of class-mates

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const icons = ["bars", "bug", "bowling-ball", "coffee", "couch", "football-ball", "gem", "laptop"];
+const icons = ["images/david.png", "images/robert.png", "images/garret.png"];
 const btnStart = document.querySelector('.btnStart');
 const gameOverEle = document.getElementById('gameOverEle');
 const container =document.getElementById('container');
@@ -6,6 +6,14 @@ let gamePlay = false;
 let player;
 let animateGame;
 const box = document.querySelector('.box');
+const base = document.querySelector('.base');
+const scoreDash = document.querySelector('.scoreDash');
+const progressbar = document.querySelector('.progress-bar');
+const gamePlayArea = document.querySelector('.gamePlayArea');
+
+// var rect=box.getBoundingClientRect();
+// console.log(rect);
+
 const boxCenter = [box.offsetLeft + (box.offsetWidth/2),
     box.offsetTop + (box.offsetHeight/2)
 ];
@@ -15,14 +23,100 @@ container.addEventListener('mousedown',mouseDown);
 container.addEventListener('mousemove', movePosition);
 
 
+function startGame() {
+    gamePlay = true;
+    gameOverEle.style.display = 'none';
+    player = {
+        score: 0,
+        barwidth: 100,
+        lives: 100
+    }
+    setupBadguys(6)
+    animateGame = requestAnimationFrame(playGame)
+}
+
+function moveEnemy() {
+    let tempEnemy = document.querySelectorAll('.baddy');
+    let hitter = false;
+    let tempShots= document.querySelectorAll('.fireme');
+    for (let enemy of tempEnemy) {
+        if (enemy.offsetTop > 800 || enemy.offsetTop < 0 || enemy.offsetLeft > 1000 || enemy.offsetLeft < 0) {
+            enemy.parentNode.removeChild(enemy);
+            badmaker()
+        }
+        else {
+            enemy.style.top = enemy.offsetTop + enemy.movery + 'px';
+            enemy.style.left = enemy.offsetLeft + enemy.moverx + 'px';
+
+            for(let shot of tempShots){
+                if(isCollide(shot,enemy)&& gamePlay){
+                    console.log('hit');
+                    player.score += enemy.points;
+                    enemy.parentNode.removeChild(enemy);
+                    shot.parentNode.removeChild(shot);
+                    updateDash();
+                    badmaker();
+                    break;
+                }
+            }
+        }
+        if (isCollide(box, enemy)) {
+            hitter = true;
+            player.lives--;
+            if (player.lives < 0){gameOver();
+            }
+        }
+        
+    }
+    if(hitter){
+        base.style.backgroundColor = 'red';
+        hitter = false;
+    }
+    else{
+        base.style.backgroundColor = '';
+    }
+}
+
+function gameOver(){
+    cancelAnimationFrame(animateGame);
+    gameOverEle.style.display = 'block';
+    gameOverEle.querySelector('span').innerHTML = 'GAME OVER <br> Your Score is ' +player.score;
+    gamePlay = false;
+    let tempEnemy = document.querySelectorAll('.baddy');
+    for(let enemy of tempEnemy){
+        enemy.parentNode.removeChild(enemy);
+    }
+    let tempShots = document.querySelectorAll('.fireme');
+    for (let shot of tempShots) {
+        shot.parentNode.removeChild(shot);
+    }
+}
+
+function updateDash() {
+    scoreDash.innerHTML = player.score;
+    let tempPer = (player.lives / player.barwidth)*100 +'%';
+    progressbar.style.width = tempPer;
+}
+
 function movePosition(e){
     let deg = getDeg(e);
-    let mouseAngle = getDeg(e);
-    box.style.transform = 'rotate('+deg+'deg)';
+    // document.body.style.backgroundImage = "url('turretGame/turretGameEdited/images/loopmandan.png')";
+    box.style.webkitTransform = 'rotate(' + deg + 'deg)';
+    box.style.mozTransform = 'rotate(' + deg + 'deg)';
+    box.style.msTransform = 'rotate(' + deg + 'deg)';
+    box.style.oTransform = 'rotate(' + deg + 'deg)';
+    box.style.transform = 'rotate(' + deg + 'deg)';
+}
+
+function isCollide(a , b) {
+    let aRect = a.getBoundingClientRect();
+    let bRect = b.getBoundingClientRect();
+    return!(
+         (aRect.bottom < bRect.top) || (aRect.top > bRect.bottom) || (aRect.right < bRect.left) || (aRect.left > bRect.right))  
 }
 
 function getDeg(e){
-    let angle = Math.atan2(e.clientX - boxCenter[0], -(e.clientY - boxCenter[1]));
+    let angle = Math.atan2(e.clientX - boxCenter[0] - gamePlayArea.offsetLeft, -(e.clientY - boxCenter[1]));
     return angle * (180 / Math.PI);
 
 }
@@ -35,7 +129,6 @@ function mouseDown(e) {
     if (gamePlay) {
         let div = document.createElement('div');
         let deg = getDeg(e);
-
         div.setAttribute('class', 'fireme');
         console.log('fire');
         div.moverx = 5*Math.sin(degRad(deg));
@@ -54,43 +147,25 @@ function mouseDown(e) {
     }
 }
 
-function startGame(){
-    gamePlay = true;
-    gameOverEle.style.display = 'none';
-    player = {
-        score: 0,
-        barwidth: 100,
-        lives: 100
-    }
-    setupBadguys(10);
-    animateGame = requestAnimationFrame(playGame)
-}
+
 
 function setupBadguys(num) {
-    for (let x=0; x < num; x++) {
+    for (let x = 0; x < num; x++) {
+        badmaker();
+    }
+}
+
+function randomMe(num) {
+    return Math.floor(Math.random() * num);{
         badmaker();
     }
 }
 
 
-function randomMe(num) {
-    return Math.floor(Math.random() * num);
-}
-
-function moveEnemy(){
-    let tempEnemy = document.querySelectorAll('.baddy');
-    for(let enemy of tempEnemy){
-        if(enemy.offsetTop > 50||enemy.offsetTop < 0||enemy.offsetLeft>750||enemy.offsetLeft < 0){
-            enemy.parentNode.removeChild(enemy);
-        }else{
-        enemy.style.top = enemy.offsetTop + enemy.movery + 'px';
-        enemy.style.left = enemy.offsetLeft +enemy.moverx + 'px';
-    }
-}
 
 function badmaker(){
     let div = document.createElement('div');
-    let myIcon = 'fa-' + icons[randomMe(icons.length)];
+    let myIcon =icons[randomMe(icons.length)];
     let x, y, xmove, ymove;
     let randomStart = randomMe(4);
     let dirSet = randomMe(5) + 2;
@@ -98,45 +173,58 @@ function badmaker(){
     switch (randomStart) {
         case 0:
             x = 0;
-            y = randomMe(600);
+            y = randomMe(800);
             ymove = dirPos;
             xmove = dirSet;
         break;
         case 1:
-            x = 800;
-            y = randomMe(600);
+            x = 1000;
+            y = randomMe(800);
             ymove = dirPos;
             xmove = dirSet * -1;
+            console.log('case1');
         break;
         case 2:
-            x = randomMe(800);
+            x = randomMe(1000);
             y = 0;
             ymove = dirSet;
             xmove = dirPos;
         break;
         case 3:
-            x = randomMe(800);
-            y = 600;
+            x = randomMe(1000);
+            y = 800;
             ymove = dirSet * -1;
             xmove = dirPos;
+            console.log('case3');
         break;
     }
-}
-    div.innerHTML = '<i class="fas ' + myIcon + '"></i>';
+
+    div.style.color=randomColor();
+    div.style.backgroundImage = "url('"+myIcon+"')";
     div.setAttribute('class', 'baddy');
-    div.style.fontSize = randomMe(20) + 30 + 'px';
+    div.style.height = '100px';
+    div.style.width = '100px';
     div.style.left = x + 'px';
     div.style.top = y + 'px';
     div.moverx = xmove;
     div.movery = ymove;
+    div.points = randomMe(5)+1;
     container.appendChild(div);
+}
+
+function randomColor(){
+    function c(){
+        let hex = randomMe(256).toString(16);
+        return ('0'+String(hex)).substr(-2); 
+    }
+    return '#'+c()+c()+c();
 }
 
 function moveShots(){
     let tempShots = document.querySelectorAll('.fireme');
     for(let shot of tempShots){
 
-        if(shot.offsetTop > 600 || shot.offsetTop < 0||shot.offsetLeft > 800||shot.offsetLeft < 0){
+        if(shot.offsetTop > 800 || shot.offsetTop < 0||shot.offsetLeft > 1000||shot.offsetLeft < 0){
             shot.parentNode.removeChild(shot);
         } 
         else{
@@ -150,6 +238,7 @@ function playGame(){
     if (gamePlay){
         moveShots();
         moveEnemy();
+        updateDash();
         animateGame = requestAnimationFrame(playGame);
     }
     
